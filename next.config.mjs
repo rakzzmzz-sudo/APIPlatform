bb9537973ef9231d6935f51ae854b177a2ca35f6
Rakesh <157781536+rakzzm@users.noreply.github.com>
2026-02-21 21:07:42 +0800

    fix: explicit output tracing for sqlite on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/dev.db'],
+    }
+  }
 };
 
 export default nextConfig;
